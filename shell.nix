b5cab230e023c8285454746526a1ddfc4e3c47c3
Handré Stolp <HanStolpo@gmail.com>
2017-07-07 10:23:26 +0200

    Use nix

---
+++ shell.nix
@@ -0,0 +1,25 @@
+let
+  pkgs = import <nixpkgs> {};
+  haskellPackages = pkgs.haskellPackages.override {
+    overrides = self: super: with pkgs.haskell.lib;{
+      ghc-syb-utils = dontCheck super.ghc-syb-utils;
+  };};
+  ghcWithPackages =
+    haskellPackages.ghcWithHoogle (g: with g;
+    [classy-prelude
+     hakyll
+     hakyll-favicon hakyll-filestore
+     hakyll-ogmarkup hakyll-series
+     base
+    ]);
+in with pkgs;
+  runCommand "hakyll-env"
+    (with ghcWithPackages.haskellPackages;
+      rec
+        { ghc = ghcWithPackages;
+          shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+          buildInputs =
+            [ ghcWithPackages zsh ghc-mod hindent cabal-helper
+              cabal-install codex stylish-haskell hoogle hlint
+              align netcat-openbsd hdevtools];})
+  "echo success > $out"
